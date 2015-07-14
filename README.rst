3571d3451fc305e1eaf733df30248c0a17a8a004
Rob <grovesr1@yahoo.com>
2015-07-14 07:24:55 -0400

    Initial commit of IMS, abstracted from redcross project

---
+++ README.rst
@@ -0,0 +1,32 @@
+=====
+IMS
+=====
+
+IMS is a simple Django app to for managing inventory.  IMS stands for
+Inventory Management System.  The base models are:
+Site - location where inventory is stored
+ProductInformation - description of inventory types
+InventoryItem - record recording the status of a product at a site
+
+Detailed documentation is in the "docs" directory.
+
+Quick start
+-----------
+
+1. Add "ims" to your INSTALLED_APPS setting like this::
+
+    INSTALLED_APPS = (
+        ...
+        'ims',
+    )
+
+2. Include the ims URLconf in your project urls.py like this::
+
+    url(r'^ims/', include('ims.urls')),
+
+3. Run `python manage.py migrate` to create the ims models.
+
+4. Start the development server and visit http://127.0.0.1:8000/admin/
+   to administer the ims models (you'll need the Admin app enabled).
+
+5. Visit http://127.0.0.1:8000/ims/ to work with inventory.
---
+++ setup.py
@@ -0,0 +1,33 @@
+import os
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-ims',
+    version='1.0',
+    packages=['ims'],
+    include_package_data=True,
+    license='3-BSD License',  # example license
+    description='django-ims is a simple Django app to for managing inventory',
+    long_description=README,
+    url='http://www.example.com/',
+    author='Rob Groves',
+    author_email='grovesr1@yahoo.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Python',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License', # example license
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        # Replace these appropriately if you are stuck on Python 2.
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
