ff0bb4d55786298c8e3923dbade57a06fc8de16b
Rob <grovesr1@yahoo.com>
2015-07-19 15:35:36 -0400

    version 1.2.0 full separate app version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-ims',
-    version='1.0',
+    version='1.2',
     packages=['ims'],
     include_package_data=True,
     license='3-BSD License',  # example license
